d5080d3e02638c9ea4c3bd0309804e0bc2cab673
KOUSHIK <chk240404@gmail.com>
2023-12-05 10:58:33 +0530

    random

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,14 +1,18 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
-    name: String,
-    price: Number,
-    quantity: String,
-    src: String,
-    productType: String,
-    petType: String,
-})
+  name: String,
+  price: Number,
+  quantity: String,
+  src: String,
+  productType: String,
+  petType: String,
+  breed_group: {
+    type: String,
+    required: false,
+  },
+});
 
-const Product = mongoose.model("Product", productSchema)
+const Product = mongoose.model("Product", productSchema);
 
 export default Product;
