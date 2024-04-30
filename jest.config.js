bb7c741b3b7a62c7609a098cffc9d994046c9d69
KOUSHIK <chk240404@gmail.com>
2024-04-30 16:18:45 +0530

    "checking jest"

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,19 @@
+// export default {
+//   // other configurations...
+//   testEnvironment: "node",
+//   transform: {
+//     "^.+\\.jsx?$": "babel-jest",
+//   },
+//   moduleNameMapper: {
+//     "^@testing-library/react$": "@testing-library/react/esm",
+//   },
+// };
+
 export default {
   // other configurations...
   testEnvironment: "node",
   transform: {
     "^.+\\.jsx?$": "babel-jest",
   },
-  moduleNameMapper: {
-    "^@testing-library/react$": "@testing-library/react/esm",
-  },
+  extensionsToTreatAsEsm: [".jsx"],
 };
