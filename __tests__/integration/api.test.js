ddb6e75e9b057c7d8f840cb4484534085ace6409
KOUSHIK <chk240404@gmail.com>
2024-04-30 15:02:35 +0530

    "Dummy commit"

---
+++ __tests__/integration/api.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import Button from "./Button";
+
+test("renders button with correct text", () => {
+  render(<Button label="Click me" />);
+  const buttonElement = screen.getByText(/click me/i);
+  expect(buttonElement).toBeInTheDocument();
+});
